ffe114896bfad678af9d6a2c2eaaedc4a3712be4
resteve <resteve@zikzakmedia.com>
2014-11-18 18:50:00 +0100

    Migrate test version 3.4

--- tests/test_account_payment_es_csb_34_01.py
+++ tests/test_account_payment_es_csb_34_01.py
@@ -1,31 +1,22 @@
 # This file is part of account_payment_es_csb_34_01 module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from trytond.tests.test_tryton import test_depends
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
+import doctest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class AccountPaymentEsCSB3401TestCase(unittest.TestCase):
-    '''
-    Test Account Payment ES CSB 34-01 module.
-    '''
+    '''Test Account Payment ES CSB 34-01 module'''
 
     def setUp(self):
         trytond.tests.test_tryton.install_module(
             'account_payment_es_csb_34_01')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        '''Test depends'''
         test_depends()
 
 
@@ -34,6 +25,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountPaymentEsCSB3401TestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
