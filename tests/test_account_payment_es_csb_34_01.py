1d1c53a5daa2ca159656c3e26656021b95dece22
resteve <resteve@zikzakmedia.com>
2015-11-29 11:56:21 +0100

    pyflakes

--- tests/test_account_payment_es_csb_34_01.py
+++ tests/test_account_payment_es_csb_34_01.py
@@ -1,22 +1,24 @@
-# This file is part of account_payment_es_csb_34_01 module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-import unittest
-import doctest
-import trytond.tests.test_tryton
+#!/usr/bin/env python
+# This file is part of the account_payment_es_csb_34_01 module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+import trytond.tests.test_tryton
+import unittest
 
 
 class AccountPaymentEsCSB3401TestCase(unittest.TestCase):
     '''Test Account Payment ES CSB 34-01 module'''
 
     def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'account_payment_es_csb_34_01')
+        trytond.tests.test_tryton.install_module('account_payment_es_csb_34_01')
+
+    def test0005views(self):
+        'Test views'
+        test_view('account_payment_es_csb_34_01')
 
     def test0006depends(self):
-        '''Test depends'''
+        'Test depends'
         test_depends()
 
 
